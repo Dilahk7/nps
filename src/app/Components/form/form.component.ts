865f9f49df587edfb73fea456013d90308ccbee4
khalid <37486526+Dilahk7@users.noreply.github.com>
2022-03-04 12:20:20 +0530

    Increased func

--- src/app/Components/form/form.component.ts
+++ src/app/Components/form/form.component.ts
@@ -1,54 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,FormGroup ,Validators} from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  isDisabled = true;
 
-  isDisabled=true;
-  basicForm=new FormGroup(
-    {
-      salary:new FormControl({value:1000,disabled: true}),
-      nps_percent:new FormControl(0,[
+  dropvalues = [10, 20, 30, 40];
 
-        Validators.required,
+  basicForm = new FormGroup({
+    dropdownval: new FormControl(30000),
+    salary: new FormControl(),
+    nps_percent: new FormControl(10, [
+      Validators.required,
 
-        Validators.pattern('^[0-9]*$'),
+      Validators.pattern('^[0-9]*$'),
 
-        Validators.min(0),
-        Validators.max(100),
+      Validators.min(0),
+      Validators.max(100),
+    ]),
+    finalValue: new FormControl({
+      value: 0,
+      disabled: true,
+    }),
+    pranNumber: new FormControl({ value: '', disabled: true }),
+  });
 
-      ]),
-      finalValue: new FormControl({value:'0',disabled: true}),
-      pranNumber:new FormControl({value:'',disabled: true})
-    }
-  )
+  constructor() {}
 
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onSubmit(){
+  onSubmit() {
     //console.warn(this.basicForm.value);
-   // console.log(this.basicForm.controls['firstName'].value);
-   // console.log(this.basicForm.controls['nps_percent')?.value);
+    // console.log(this.basicForm.controls['firstName'].value);
+    // console.log(this.basicForm.controls['nps_percent')?.value);
   }
 
-  onCheck(){
+  onCheck() {
+    // const percent_dropdown = this.basicForm.get('nps_percent')?.value;
 
-   const percent=this.basicForm.get('nps_percent')?.value;
-   const salary=this.basicForm.get('salary')?.value;
-   
-   const nps=(salary*percent)/100;
-   // this.basicForm.setControl('finalValue',nps);
-   this.basicForm.controls['finalValue'].setValue(nps);
-   console.log(nps); 
+    let salary;
+    const percent = this.basicForm.get('nps_percent')?.value;
+    if (!this.checkopt()) {
+      salary = 300000;
+    } else {
+      salary = this.basicForm.get('salary')?.value;
+    }
+    console.log(percent);
 
+    let nps = (salary * percent) / 100;
+    // this.basicForm.setControl('finalValue',nps);
+    nps = Math.floor(nps);
+    this.basicForm.controls['finalValue'].setValue(nps);
+    console.log(nps);
   }
-
+  checkopt() {
+    return this.basicForm.get('dropdownval')?.value == 'User-input';
+  }
 }
